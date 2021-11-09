1fd610c389ddd0ce78cc99a8609066ec1725d640
Paweł Zav <zawadzki.pawel@protonmail.com>
2021-11-09 14:37:43 +0100

    Dodaj pliki projektów.

---
+++ RejestrKontroli1/DAL/RejestrContext.cs
@@ -0,0 +1,22 @@
+﻿using RejestrKontroli1.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RejestrKontroli1.DAL
+{
+    public class RejestrContext:DbContext
+    {
+       
+            public RejestrContext() : base("connectionstring")
+            {
+
+
+            }
+            public DbSet<Rejestr> Rejestr { get; set; }
+
+        
+    }
+}
